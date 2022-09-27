a5270a07a4ec676d52cd6f2c5bab3d93e9d6100f
Amer Farooq <amerfarooq@live.com>
2022-09-27 15:00:58 +0500

    chore: removed unused code

--- src/demo_mainnet.ts
+++ src/demo_mainnet.ts
@@ -1,66 +1,109 @@
-
-import { Connection, Keypair, PublicKey,} from "@solana/web3.js";
-import {fetchAllPoolKeys, fetchPoolKeys, getRouteRelated} from "./util_mainnet"
-import { getTokenAccountsByOwner, swap, addLiquidity, removeLiquidity, routeSwap, tradeSwap, getLiquidityInfo } from "./util";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import {
+  fetchAllPoolKeys,
+  fetchPoolKeys,
+  getRouteRelated,
+} from "./util_mainnet";
+import {
+  getTokenAccountsByOwner,
+  swap,
+  addLiquidity,
+  removeLiquidity,
+  routeSwap,
+  tradeSwap,
+  getLiquidityInfo,
+} from "./util";
+import {
+  TOKEN_PROGRAM_ID,
+  SPL_ACCOUNT_LAYOUT,
+  TokenAccount,
+  LiquidityPoolKeys,
+  Liquidity,
+  Route,
+  Trade,
+  TokenAmount,
+  Token,
+  Percent,
+  Currency,
+  LIQUIDITY_STATE_LAYOUT_V4,
+} from "@raydium-io/raydium-sdk";
+import { OpenOrders } from "@project-serum/serum";
 
 // @ts-ignore
-import bs58 from "bs58"
+import bs58 from "bs58";
 import { SERUM_PROGRAM_ID_V3 } from "@raydium-io/raydium-sdk";
 
+const fetchPoolPrice = async (
+  connection: Connection,
+  poolID: string,
+  log: boolean = false
+): Promise<number> => {
+  
+  const poolPubKey = new PublicKey(poolID);
+  const info = await connection.getAccountInfo(poolPubKey);
+  if (info === null) {
+    throw new Error("Could not connect to the pool!");
+  }
+  const state = LIQUIDITY_STATE_LAYOUT_V4.decode(info.data);
+  const baseTokenAmount = await connection.getTokenAccountBalance(
+    state.baseVault
+  );
+  if (log) console.log("Base Token Amount: ", baseTokenAmount);
+
+  const quoteTokenAmount = await connection.getTokenAccountBalance(
+    state.quoteVault
+  );
+  if (log) console.log("Quote Token Amount: ", quoteTokenAmount);
+  const openOrders = await OpenOrders.load(
+    connection,
+    state.openOrders,
+    SERUM_PROGRAM_ID_V3
+  );
+  if (log) console.log("Open orders: ", openOrders);
+
+  const baseDecimal = 10 ** state.baseDecimal.toNumber();
+  const quoteDecimal = 10 ** state.quoteDecimal.toNumber();
+  if (log) console.log("Base decimal: ", baseDecimal);
+  if (log) console.log("Quote decimal: ", quoteDecimal);
+
+  const openOrdersTotalBase = openOrders.baseTokenTotal.toNumber() / baseDecimal;
+  if (log) console.log("openOrdersTotalBase: ", openOrdersTotalBase);
+
+  const openOrdersTotalQuote = openOrders.quoteTokenTotal.toNumber() / quoteDecimal;
+  if (log) console.log("openOrdersTotalQuote: ", openOrdersTotalQuote);
+
+  const basePnl = state.baseNeedTakePnl.toNumber() / baseDecimal;
+  if (log) console.log("basePnl: ", basePnl);
+
+  const quotePnl = state.quoteNeedTakePnl.toNumber() / quoteDecimal;
+  if (log) console.log("quotePnl: ", quotePnl);
+
+  const base = baseTokenAmount.value.uiAmount! + openOrdersTotalBase - basePnl;
+  if (log) console.log("base: ", base);
+
+  const quote = quoteTokenAmount.value.uiAmount! + openOrdersTotalQuote - quotePnl;
+  if (log) console.log("quote: ", quote);
+
+  if (log) console.log("Price: ", quote / base);
+  return quote / base;
+};
 
 (async () => {
-    const connection = new Connection("https://solana-api.projectserum.com", "confirmed");
+  const connection = new Connection(
+    "https://solana-api.projectserum.com",
+    "confirmed"
+  );
 
-    // change to your privateKey
-    const secretKey = bs58.decode('3qswEeCJcA9ogpN3JEuXBtmnU35YPzSxBwzrk6sdTPhogMJ64WuabU9XWg2yUegJvv1qupYPqo2jQrrK26N7HGsD')
+  const secretKey = bs58.decode(
+    "2vTyWKgs2NZdzoJzfmnVARrzofHiWojZ8bG5JuczUouTPswy99P36CeurbPYfG7ijdniT6gqjfXCWD5dVdN5uJac"
+  );
 
-    const ownerKeypair = Keypair.fromSecretKey( secretKey )
-    const owner = ownerKeypair.publicKey;
-    console.log(owner.toString());
-    
-    const tokenAccounts = await getTokenAccountsByOwner(connection, owner)
+  const raydium = "7XawhbbxtsRcQA8KTkHT9f9nc6d69UwqCDh6U5EEbEmX";
+  const orca = "HWHvQhFmJB3NUcu1aihKmrKegfVxBEHzwVX6yZCKEsi1";
 
-    // SOL-USDC
-    const POOL_ID = "58oQChx4yWmvKdwLLZzBi4ChoCc2fqCUWBkwMihLYQo2"
+  while (true) {
+    // console.log("Orca: SOL-USDT", await fetchPoolPrice(connection, orca));
+    console.log("Raydium: SOL-USDT", await fetchPoolPrice(connection, raydium));
+  }
 
-    const info = await getLiquidityInfo(connection, new PublicKey(POOL_ID), SERUM_PROGRAM_ID_V3)
-
-    // @ts-ignore
-    console.log("base Vault:", info.baseVaultKey.toBase58(), info.baseVaultBalance);
-    // @ts-ignore
-    console.log("quote Vault:", info.quoteVaultKey.toBase58(), info.quoteVaultBalance);
-  
-    // @ts-ignore
-    console.log(`openOrders Account ${info.openOrdersKey.toBase58()}, base Serum book total: ${info.openOrdersTotalBase},quote Serum book total: ${info.openOrdersTotalQuote} `)
-  
-      // @ts-ignore
-    console.log(`base pnl: ${info.basePnl }, quote pnl: ${info.quotePnl}` );
-
-    // @ts-ignore
-    console.log(`Final base:${info.base}, quote:${info.quote}, priceInQuote: ${info.priceInQuote}, lpSupply:${info.lpSupply}` )
-
-    
-    // const allPoolKeys = await fetchAllPoolKeys(connection);
-    // const poolKeys = allPoolKeys.find((item) => item.id.toBase58() === RAY_USDC)
-    const poolKeys = await fetchPoolKeys(connection, new PublicKey(POOL_ID))
-
-
-    await swap(connection, poolKeys, ownerKeypair, tokenAccounts)
-
-    await addLiquidity(connection, poolKeys, ownerKeypair, tokenAccounts)
-
-    await removeLiquidity(connection, poolKeys, ownerKeypair, tokenAccounts)
-
-    const FIDA_RAY = "2dRNngAm729NzLbb1pzgHtfHvPqR4XHFmFyYK78EfEeX"
-    const RAY_USDC = "6UmmUiYoBjSrhakAobJw8BvkmJtDVxaeBtbt7rxWo1mg"
-
-    const fromPoolKeys = await fetchPoolKeys(connection, new PublicKey(FIDA_RAY))
-    const toPoolKeys = await fetchPoolKeys(connection, new PublicKey(RAY_USDC))
-    const FIDA_MINT_ID = fromPoolKeys.baseMint;
-    const USDC_MINT_ID = poolKeys.quoteMint;
-    const relatedPoolKeys = await getRouteRelated(connection, FIDA_MINT_ID, USDC_MINT_ID)
-
-    await routeSwap(connection, fromPoolKeys, toPoolKeys, ownerKeypair, tokenAccounts)
-
-    await tradeSwap(connection, FIDA_MINT_ID, USDC_MINT_ID, relatedPoolKeys, ownerKeypair, tokenAccounts)
-})()
+})();
